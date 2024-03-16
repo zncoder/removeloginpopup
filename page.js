c740d78b29f92eb2d58788f7ab51d6a4b3c71a2c
Bin Zhang <zncoder@gmail.com>
2024-03-16 05:30:20 -0700

    keep running in background

--- page.js
+++ page.js
@@ -1,40 +1,35 @@
-function removeLoginPopup(nth) {
-  let el = findPopup()
-  if (el) {
-	console.log('found', el)
-	el.parentElement.removeChild(el)
-	return
-  }
-  if (nth < 2) {
-	setTimeout(removeLoginPopup, 5001, nth+1)
-  } else {
-	console.log('login popup not found')
-  }
+function removeLoginPopup() {
+	let el = findPopup()
+	if (el) {
+		el.parentElement.removeChild(el)
+	}
+	// keep running in background, since
+	// some site keeps enabling the login popup
+	setTimeout(removeLoginPopup, 5001)
 }
 
 function findPopup() {
-  // if (location.hostname.endsWith(".expedia.com")) {
-  // 	return findExpediaPopup()
-  // }
-  return findGooglePopup()
+	// if (location.hostname.endsWith(".expedia.com")) {
+	// 	return findExpediaPopup()
+	// }
+	return findGooglePopup()
 }
 
 function findExpediaPopup() {
-  return document.querySelector('.uitk-menu-open')
+	return document.querySelector('.uitk-menu-open')
 }
 
 function findGooglePopup() {
-  let iframes = document.querySelectorAll('iframe')
-  for (let x of iframes) {
-	if (x.src.startsWith('https://accounts.google.com/gsi/iframe/select?')) {
-	  return x
-	} else if (x.src.startsWith('https://ogs.google.com/widget/callout')) {
-	  // google map
-	  return x
+	let iframes = document.querySelectorAll('iframe')
+	for (let x of iframes) {
+		if (x.src.startsWith('https://accounts.google.com/gsi/iframe/select?')) {
+			return x
+		} else if (x.src.startsWith('https://ogs.google.com/widget/callout')) {
+			// google map
+			return x
+		}
 	}
-  }
-  return null
+	return null
 }
 
-console.log('removeloginpopup')
-setTimeout(removeLoginPopup, 1001, 0)
+setTimeout(removeLoginPopup, 1001)
